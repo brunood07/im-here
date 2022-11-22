ea6e24d44d3fa5cfb3831a3673ba7828d5744373
Bruno Domingues <bruno.domingues@lighthouseit.com.br>
2022-11-22 13:14:08 -0300

    feat: participant component created

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,34 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
+import { Participant } from "../../components/Participant";
 import { styles } from "./styles";
 
 export default function Home() {
+  const handleParticipantAdd = () => {
+    console.log("Você clicou no botão de adicionar!");
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
 
       <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Participant />
+      <Participant />
+      <Participant />
     </View>
   );
 }
